8d32ab6c3315a308f9efd7340de9c772492cf001
itsHikariii <hasonanthonynugroho88@gmail.com>
2024-10-10 18:20:07 +0700

    Open the PartsManager.cs file in the Data folder.
    Add BaseAddress and Url static fields as defined in the following code snippet to the PartsManager class. Replace the text URL GOES HERE with the URL of the REST web service that you noted earlier:

--- src/client/PartsClient/PartsClient/Data/PartsManager.cs
+++ src/client/PartsClient/PartsClient/Data/PartsManager.cs
@@ -13,7 +13,7 @@ public static class PartsManager
     // TODO: Add fields for BaseAddress, Url, and authorizationKey
     static readonly string BaseAddress = "URL GOES HERE";
     static readonly string Url = $"{BaseAddress}/api/";
-
+    
     static HttpClient client;
 
     private static async Task<HttpClient> GetClient()
